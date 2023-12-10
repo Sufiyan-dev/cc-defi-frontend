1f430a8bc9b661b560b6d4a7cef77cf29b7cff5a
Sufiyan <sufiyanmemon687@gmail.com>
2023-12-10 17:21:05 -0500

    uupdated css

--- src/Components/BuyBack.js
+++ src/Components/BuyBack.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
-import "./BuyBack.css"
+import React, { useState } from 'react';
+import "./BuyBack.css";
 
 const tokenSample = [
-    { token: "0xbkbkjbjkjk", tokenName: "ETH", tokenBalance: 10 ** 18 },
-    { token: "0xbkbkjbdfsdjkjk", tokenName: "ETH", tokenBalance: 10 ** 18 },
-    { token: "0xbkbkjbjfsdffkjk", tokenName: "ETH", tokenBalance: 10 ** 18 },
-]
+  { token: "0xbkbkjbjkjk", tokenName: "ETH", tokenBalance: 10 ** 18 },
+  { token: "0xbkbkjbdfsdjkjk", tokenName: "ETH", tokenBalance: 10 ** 18 },
+  { token: "0xbkbkjbjfsdffkjk", tokenName: "ETH", tokenBalance: 10 ** 18 },
+];
 
 const BuyBack = () => {
   const [reserves, setReserves] = useState(tokenSample);
@@ -15,25 +15,27 @@ const BuyBack = () => {
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    console.log(selectedToken)
-  }
+    console.log(selectedToken);
+  };
 
   return (
-    <div>
-        <div>Buy Back</div>
-        <div className='error-wrapper'>{error.length > 0 ? error : ""}</div>
-        <div>
-            <form onSubmit={handleOnSubmit}>
-                <select onChange={(e) => setSelectedToken(e.target.value)}>
-                    {reserves.length > 0 ? reserves.map((reserve) => (
-                        <option value={reserve.token} key={reserve.token}>{reserve.tokenName}</option>
-                    )) : <div></div>}
-                </select>
-                <button className="subit-btn" type='submit'>{qouted ? "Buy" : "Qoute"}</button>
-            </form>
-        </div>
+    <div className='buyback-main'>
+      <div className='buyback-wrapper'>
+        <div className='buyback-title'>Buy Back</div>
+        <div className='buyback-error'>{error.length > 0 ? error : ""}</div>
+        <form onSubmit={handleOnSubmit}>
+          <div className='buyback-dropdown'>
+            <select onChange={(e) => setSelectedToken(e.target.value)}>
+              {reserves.length > 0 ? reserves.map((reserve) => (
+                <option value={reserve.token} key={reserve.token}>{reserve.tokenName}</option>
+              )) : <div></div>}
+            </select>
+            <button className="buyback-btn" type='submit'>{qouted ? "Buy" : "Quote"}</button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BuyBack
+export default BuyBack;
