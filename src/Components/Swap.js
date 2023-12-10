de2dc8adca21315246be3997a7600d9da69319c8
Sufiyan <sufiyanmemon687@gmail.com>
2023-12-10 15:20:38 -0500

    completed swap page

--- src/Components/Swap.js
+++ src/Components/Swap.js
@@ -14,6 +14,7 @@ const Swap = () => {
     const [tokenOut, setTokenOut] = useState(tokenSample[0].token);
     const [tokenOutAmount, setTokenOutAmount] = useState(0);
     const [qouted, setQouted] = useState(false);
+    const [slippage, setSlippage] = useState(0.3);
 
     const handleOnSubmitForm = (e) => {
         e.preventDefault();
@@ -22,6 +23,7 @@ const Swap = () => {
         }
         if(qouted) {
             // swap logic
+            console.log("swapppp")
         } else {
             // qoute 
             handleGetQoute();
@@ -53,34 +55,42 @@ const Swap = () => {
         setTokenOutAmount(0);
     }
 
+    const handleOnChangeSlippage = (e) => {
+        const slippage = e.target.value;
+        if(slippage > 100){
+            return alert("cannot go above 100%");
+        } 
+        setSlippage(e.target.value);
+    }
+
     return (
         <div>
             <div className='swap-title'>Swap</div>
             <div className='swap-main'>
                 <div className='swap-wrapper'>
                     <form onSubmit={handleOnSubmitForm}>
-                    `<div className='swap-tokein'>
-                        <input className='tokein-amount' type='number' value={tokenInAmount} onChange={handleOnChangeTokenInAmount}/>
-                        <select onChange={handleOnChangeTokenIn}>
-                            {tokens.map((token) => (
-                                <option key={token.token} value={token.token}>{token.tokenName}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className='swap-tokeout'>
-                        <input className='tokeout-amount' type='number' value={tokenOutAmount} disabled={true}/>
-                        <select onChange={handleOnChangeTokenOut}>
-                            {tokens.map((token) => (
-                                <option key={token.token} value={token.token}>{token.tokenName}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className='swap-slippage-wrapper'>
-                        <
-                    </div>
-                    <div>
-                        <button type='submit'>{qouted ? "Swap" : "Qoute"}</button>
-                    </div>
+                        `<div className='swap-tokein'>
+                            <input className='tokein-amount' type='number' value={tokenInAmount} onChange={handleOnChangeTokenInAmount}/>
+                            <select onChange={handleOnChangeTokenIn}>
+                                {tokens.map((token) => (
+                                    <option key={token.token} value={token.token}>{token.tokenName}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className='swap-tokeout'>
+                            <input className='tokeout-amount' type='number' value={tokenOutAmount} disabled={true}/>
+                            <select onChange={handleOnChangeTokenOut}>
+                                {tokens.map((token) => (
+                                    <option key={token.token} value={token.token}>{token.tokenName}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className='swap-slippage-wrapper'>
+                            <input type='number' value={slippage} onChange={handleOnChangeSlippage}/>
+                        </div>
+                        <div>
+                            <button type='submit'>{qouted ? "Swap" : "Qoute"}</button>
+                        </div>
                     </form>
                 </div>
             </div>
